42b5e1e07ae077e5f54d4abc38625fcf208f6ed5
hodongman <jhd9206@gmail.com>
2018-07-28 15:43:44 +0900

    make histogram update

--- highlight/views.py
+++ highlight/views.py
@@ -1,3 +1,4 @@
+import numpy as np
 from rest_framework import generics
 
 from .models import (
@@ -34,4 +35,32 @@ class VideoHighlightDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = VideoHightlightSerializer
     name = 'video-hightlight-detail'
 
+class ResponseHighlightSet(generics.ListAPIView):
+
+    serializer_class = VideoHightlightSerializer
+    name = 'responsehighlight-videoid-list'
+
+    def get_queryset(self):
+
+        video_id = self.kwargs['videoid']
+        response_list = VideoInteraction.objects.filter(video_id=video_id)
+        pointed_time_list = [item.pointed_time for item in response_list]
+        VideoInteraction.objects.filter(video_id=video_id).delete()
+
+        resp = makeHistogramByResponseTime(video_id, pointed_time_list)
+        VideoHightlight.objects.bulk_create(resp)
+        return VideoHightlight.objects.filter(video_id=video_id)
+
+def makeHistogramByResponseTime(video_id, pointed_time_list):
+
+    hist, bin_edges = np.histogram(pointed_time_list)
+    average = sum(hist) / len(hist)
+
+    resp = []
+    for item, value in zip(hist, bin_edges):
+        if item >= average:
+            temp = VideoHightlight(video_id=video_id, start_time=value, end_time=value)
+            resp.append(temp)
+
+    return resp
 
